4e09da18b09ffbc1225d9d003fbbe3043c273115
JonasJacobsUserspace <59708720+JonasJacobsUserspace@users.noreply.github.com>
2020-05-07 17:36:31 +0200

    Added API bash file

---
+++ TestApplication.sh
@@ -0,0 +1,6 @@
+!/bin/bash
+## curl-example
+## - submits form with csrf protection
+## version 0.0.1 - initial
+##################################################
+curl "http://worldtimeapi.org/api/timezone"
