8c18ddbb801ccb2389add771791c9f43ae239ac7
JonasJacobsUserspace <59708720+JonasJacobsUserspace@users.noreply.github.com>
2020-05-07 17:48:59 +0200

    Added comment and changed

--- TestApplication.bash
+++ TestApplication.bash
@@ -1,6 +1,16 @@
-!/bin/bash
+#!/bin/bash
 ## curl-example
 ## - submits form with csrf protection
 ## version 0.0.1 - initial
 ##################################################
-curl "http://worldtimeapi.org/api/timezone"
+websiteVariable="http://worldtimeapi.org/api/timezone"
+
+echo $websiteVariable
+
+entireURL="curl -X POST -H "$cacheVariable1" -H "$cacheVariable2" -d "$parametersVariable" "$websiteVariable""
+
+echo "Entire URL IS: $entireURL"
+
+result=`$entireURL`
+
+eval $entireURL
