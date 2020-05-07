42921be7953218b344e6377d1430193175b5c29a
JonasJacobsUserspace <59708720+JonasJacobsUserspace@users.noreply.github.com>
2020-05-07 17:50:51 +0200

    Update TestApplication.bash

--- TestApplication.bash
+++ TestApplication.bash
@@ -3,14 +3,4 @@
 ## - submits form with csrf protection
 ## version 0.0.1 - initial
 ##################################################
-websiteVariable="http://worldtimeapi.org/api/timezone"
-
-echo $websiteVariable
-
-entireURL="curl -X POST -H "$cacheVariable1" -H "$cacheVariable2" -d "$parametersVariable" "$websiteVariable""
-
-echo "Entire URL IS: $entireURL"
-
-result=`$entireURL`
-
-eval $entireURL
+curl "http://worldtimeapi.org/api/timezone"
