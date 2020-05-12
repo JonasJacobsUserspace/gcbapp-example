c6ec0bd550421391b5021d8b06f131cd37c041aa
JonasJacobsUserspace <59708720+JonasJacobsUserspace@users.noreply.github.com>
2020-05-12 09:37:09 +0200

    Added global install gcloud

--- scripts/test.bash
+++ scripts/test.bash
@@ -1,6 +1,6 @@
 # ./scripts/test.bash
 
 # Retrieve Deploy url
-npm install --save-dev
+npm install -g glcoud
 deploy_url=$(gcloud run services describe smartlockshop-backend-staging --platform managed --region europe-west1 --format 'value(status.url)')
 echo $url
