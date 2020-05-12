04dc928b8eedd77d591b42020221f689a5c1e902
JonasJacobsUserspace <59708720+JonasJacobsUserspace@users.noreply.github.com>
2020-05-12 10:03:15 +0200

    Added gloud init

--- scripts/test.bash
+++ scripts/test.bash
@@ -1,7 +1,8 @@
 # ./scripts/test.bash
 
 # Retrieve Deploy url
-curl https://dl.google.com/dl/cloudsdk/release/install_google_cloud_sdk.bash | bash
+curl https://sdk.cloud.google.com | bash
 exec -l $SHELL
+gcloud init
 deploy_url=$(gcloud run services describe smartlockshop-backend-staging --platform managed --region europe-west1 --format 'value(status.url)')
 echo $url
