e8959bdff034f9098daa4fc8d2320fe9e17b229b
JonasJacobsUserspace <59708720+JonasJacobsUserspace@users.noreply.github.com>
2020-05-12 09:44:32 +0200

    Added script to install gcloud

--- scripts/test.bash
+++ scripts/test.bash
@@ -1,6 +1,6 @@
 # ./scripts/test.bash
 
 # Retrieve Deploy url
-npm install -g glcoud
+curl https://dl.google.com/dl/cloudsdk/release/install_google_cloud_sdk.bash | bash
 deploy_url=$(gcloud run services describe smartlockshop-backend-staging --platform managed --region europe-west1 --format 'value(status.url)')
 echo $url
