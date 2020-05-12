75d18099dfa17fc0a21927d8ee8425785e9abe98
JonasJacobsUserspace <59708720+JonasJacobsUserspace@users.noreply.github.com>
2020-05-12 09:50:44 +0200

    CHange gloud command

--- scripts/test.bash
+++ scripts/test.bash
@@ -1,7 +1,5 @@
 # ./scripts/test.bash
 
 # Retrieve Deploy url
-curl https://dl.google.com/dl/cloudsdk/release/install_google_cloud_sdk.bash | bash
-gcloud config set disable_usage_reporting false
-deploy_url=$(gcloud run services describe smartlockshop-backend-staging --platform managed --region europe-west1 --format 'value(status.url)')
+deploy_url=$(/builder/google-cloud-sdk/bin/gcloud run services describe smartlockshop-backend-staging --platform managed --region europe-west1 --format 'value(status.url)')
 echo $url
