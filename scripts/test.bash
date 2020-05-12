525c48156aa2ce6b321f1bbfecab5c6637a04a9b
JonasJacobsUserspace <59708720+JonasJacobsUserspace@users.noreply.github.com>
2020-05-12 09:46:04 +0200

    Added config

--- scripts/test.bash
+++ scripts/test.bash
@@ -2,5 +2,6 @@
 
 # Retrieve Deploy url
 curl https://dl.google.com/dl/cloudsdk/release/install_google_cloud_sdk.bash | bash
+gcloud config set disable_usage_reporting false
 deploy_url=$(gcloud run services describe smartlockshop-backend-staging --platform managed --region europe-west1 --format 'value(status.url)')
 echo $url
