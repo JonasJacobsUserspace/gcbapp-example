e06f03ded665eed76d337ac2e711a677bbc74fe3
JonasJacobsUserspace <59708720+JonasJacobsUserspace@users.noreply.github.com>
2020-05-12 09:59:27 +0200

    Added restart of shell

--- scripts/test.bash
+++ scripts/test.bash
@@ -1,5 +1,7 @@
 # ./scripts/test.bash
 
 # Retrieve Deploy url
-deploy_url=$(/builder/google-cloud-sdk/bin/gcloud run services describe smartlockshop-backend-staging --platform managed --region europe-west1 --format 'value(status.url)')
+curl https://dl.google.com/dl/cloudsdk/release/install_google_cloud_sdk.bash | bash
+exec -l $SHELL
+deploy_url=$(gcloud run services describe smartlockshop-backend-staging --platform managed --region europe-west1 --format 'value(status.url)')
 echo $url
