c904d703d531226056c87a018a1eb732ce2731eb
JonasJacobsUserspace <59708720+JonasJacobsUserspace@users.noreply.github.com>
2020-05-12 09:36:23 +0200

    Added dev depencencies

--- scripts/test.bash
+++ scripts/test.bash
@@ -1,5 +1,6 @@
 # ./scripts/test.bash
 
 # Retrieve Deploy url
-deploy_url=$(npx gcloud run services describe smartlockshop-backend-staging --platform managed --region europe-west1 --format 'value(status.url)')
+npm install --save-dev
+deploy_url=$(gcloud run services describe smartlockshop-backend-staging --platform managed --region europe-west1 --format 'value(status.url)')
 echo $url
