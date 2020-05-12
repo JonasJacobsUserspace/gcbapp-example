a8225077fe8b8907b864fa3de0423d9792979458
JonasJacobsUserspace <59708720+JonasJacobsUserspace@users.noreply.github.com>
2020-05-12 10:08:48 +0200

    removed install script, added cloud builders ref

--- scripts/test.bash
+++ scripts/test.bash
@@ -1,8 +1,5 @@
 # ./scripts/test.bash
 
 # Retrieve Deploy url
-curl https://sdk.cloud.google.com | bash
-exec -l $SHELL
-gcloud init
-deploy_url=$(gcloud run services describe smartlockshop-backend-staging --platform managed --region europe-west1 --format 'value(status.url)')
+deploy_url=$(gcr.io/cloud-builders/gcloud run services describe smartlockshop-backend-staging --platform managed --region europe-west1 --format 'value(status.url)')
 echo $url
