2afb34fcc0e962678bebf154aaf2a20298e9302e
JonasJacobsUserspace <59708720+JonasJacobsUserspace@users.noreply.github.com>
2020-05-15 15:29:00 +0200

    Changed to correct reference

--- tests/test.js
+++ tests/test.js
@@ -1,6 +1,6 @@
 var request = require('supertest');
 
-var app = require('../app').app;
+var app = require('../src/app').app;
 
 describe('Calculator unit tests', function(){
     it("happy case add", function(done){
