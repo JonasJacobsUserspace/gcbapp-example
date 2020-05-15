d92c5461ed4cc8a8692aa39f7a80cd2450d3ee31
JonasJacobsUserspace <59708720+JonasJacobsUserspace@users.noreply.github.com>
2020-05-15 16:28:21 +0200

    removed listen

--- src/app.js
+++ src/app.js
@@ -1,7 +1,6 @@
 var app = require('express')();
 var router = require('../lib/routers/calcRouter');
 app.use("/calculator", router);
-app.listen(8080);
 if (process.env.NODE_ENV !== "test") {
   const port = process.env.PORT || 3000;
   app.listen(port, () => logger.info(`Listening to port ${port}...`));
